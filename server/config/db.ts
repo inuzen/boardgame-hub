0fe9c741dfe06c94ca04282ad73c30a072f3fa99
Evgenii Paikachev <condddr@gmail.com>
2022-07-20 22:32:25 +0400

    db

--- server/config/db.ts
+++ server/config/db.ts
@@ -9,9 +9,9 @@ import RoomModel from '../models/AvalonRoom';
 const db: string = process.env.DATABASE_URL || config.get('postgresURI');
 const sequelize = new Sequelize(db, {
     dialectOptions: {
-        // ssl: {
-        //     rejectUnauthorized: false,
-        // },
+        ssl: {
+            rejectUnauthorized: false,
+        },
     },
 });
 const queryInterface = sequelize.getQueryInterface();
