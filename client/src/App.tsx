65f4aa797e4cb17221de213d8960675c7c89e116
LinaKv <kvitka.p@yandex.ru>
2023-06-30 13:21:00 +0400

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import 'normalize.css';
-import './App.scss';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Welcome from './WelcomePage/Welcome';
-import AvalonGameContainer from './Games/Avalon/AvalonGameContainer';
-import { ActionScreen } from './WelcomePage/ActionScreen';
+import "normalize.css";
+import "./App.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Welcome from "./WelcomePage/Welcome";
+import AvalonGameContainer from "./Games/Avalon/AvalonGameContainer";
+import { ActionScreen } from "./WelcomePage/ActionScreen";
 
 function App() {
-    const location = useLocation();
-    return (
-        <div className="App">
-            <div className="content">
-                {/* <TransitionGroup>
+  //ggdfghdf
+  const location = useLocation();
+  return (
+    <div className='App'>
+      <div className='content'>
+        {/* <TransitionGroup>
                     <CSSTransition timeout={300} classNames="right-to-left" key={location.key}> */}
-                <Routes>
-                    <Route path="/" element={<Welcome />} />
-                    <Route path="/join" element={<ActionScreen type="join" />} />
-                    <Route path="/create" element={<ActionScreen type="create" />} />
-                    <Route path="avalon/" element={<AvalonGameContainer />}>
-                        <Route path=":roomCode" element={<AvalonGameContainer />} />
-                    </Route>
-                    <Route path="werewolf/:roomCode" element={<AvalonGameContainer />} />
-                </Routes>
-                {/* </CSSTransition>
+        <Routes>
+          <Route
+            path='/'
+            element={<Welcome />}
+          />
+          <Route
+            path='/join'
+            element={<ActionScreen type='join' />}
+          />
+          <Route
+            path='/create'
+            element={<ActionScreen type='create' />}
+          />
+          <Route
+            path='avalon/'
+            element={<AvalonGameContainer />}
+          >
+            <Route
+              path=':roomCode'
+              element={<AvalonGameContainer />}
+            />
+          </Route>
+          <Route
+            path='werewolf/:roomCode'
+            element={<AvalonGameContainer />}
+          />
+        </Routes>
+        {/* </CSSTransition>
                 </TransitionGroup> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
