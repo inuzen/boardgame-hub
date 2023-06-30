1fbbf162f3b1beb7f9fd7b0b34f3eb022eef373e
LinaKv <kvitka.p@yandex.ru>
2023-06-30 17:34:57 +0400

    new component\ checkBox

--- client/src/Games/Avalon/Components/RoleCheckbox.tsx
+++ client/src/Games/Avalon/Components/RoleCheckbox.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { CheckBox } from '../../../Components/CheckBox/CheckBox';
 import { toggleExtraRole } from '../store/avalonSlice';
 import { ROLE_LIST } from '../store/types';
-import './styles/roleCheckBox.scss';
 
 type RoleCheckboxProps = {
     roleKey: ROLE_LIST;
@@ -17,16 +17,5 @@ export const RoleCheckbox: React.FC<RoleCheckboxProps> = ({ roleKey }) => {
     const onRoleChecked = () => {
         dispatch(toggleExtraRole(roleKey));
     };
-    return (
-        <div>
-            <input
-                type="checkbox"
-                id={roleKey}
-                onChange={onRoleChecked}
-                checked={roleChecked}
-                className="customCheckbox"
-            />
-            <label htmlFor={roleKey}>{roleKeyInLowerCase}</label>
-        </div>
-    );
+    return <CheckBox id={roleKey} onChange={onRoleChecked} checked={roleChecked} text={roleKeyInLowerCase} />;
 };
