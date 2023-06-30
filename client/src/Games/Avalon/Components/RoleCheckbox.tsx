5d0874afd85e00350903510723994604d0e5ee3f
LinaKv <kvitka.p@yandex.ru>
2023-06-30 15:37:17 +0400

    roles styles and button

--- client/src/Games/Avalon/Components/RoleCheckbox.tsx
+++ client/src/Games/Avalon/Components/RoleCheckbox.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { toggleExtraRole } from '../store/avalonSlice';
 import { ROLE_LIST } from '../store/types';
+import './styles/roleCheckBox.scss';
 
 type RoleCheckboxProps = {
     roleKey: ROLE_LIST;
@@ -10,13 +11,22 @@ export const RoleCheckbox: React.FC<RoleCheckboxProps> = ({ roleKey }) => {
     const dispatch = useAppDispatch();
     const roleChecked = useAppSelector((state) => state.avalon.extraRoles.includes(roleKey));
 
+    const lastLetters = roleKey.slice(1).toLowerCase();
+    const roleKeyInLowerCase = roleKey.charAt(0) + lastLetters;
+
     const onRoleChecked = () => {
         dispatch(toggleExtraRole(roleKey));
     };
     return (
         <div>
-            <input type="checkbox" id={roleKey} onChange={onRoleChecked} checked={roleChecked} />
-            <label htmlFor={roleKey}>{roleKey}</label>
+            <input
+                type="checkbox"
+                id={roleKey}
+                onChange={onRoleChecked}
+                checked={roleChecked}
+                className="customCheckbox"
+            />
+            <label htmlFor={roleKey}>{roleKeyInLowerCase}</label>
         </div>
     );
 };
