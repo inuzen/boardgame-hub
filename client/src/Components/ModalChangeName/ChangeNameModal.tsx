fa064030fe914003af7e1e260694193b07235a01
LinaKv <kvitka.p@yandex.ru>
2023-07-02 11:17:13 +0400

    remove style from com

--- client/src/Components/ModalChangeName/ChangeNameModal.tsx
+++ client/src/Components/ModalChangeName/ChangeNameModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Modal from 'react-modal';
+import Modal, { Styles } from 'react-modal';
 import { NameInput } from '../../Games/Avalon/Components/NameInput';
 import './changeNameModal.scss';
 
@@ -7,6 +7,17 @@ type ModalProps = {
     isOpen: boolean;
     onRequestClose: () => void;
     onSetName: (newName: string) => void;
+};
+
+const style: Styles = {
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    },
 };
 
 export default function ChangeNameModal({ isOpen, onRequestClose, onSetName }: ModalProps) {
@@ -18,16 +29,7 @@ export default function ChangeNameModal({ isOpen, onRequestClose, onSetName }: M
             closeTimeoutMS={200}
             ariaHideApp={false}
             className="changeNameModal"
-            style={{
-                overlay: {
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    backgroundColor: 'rgba(255, 255, 255, 0.75)',
-                },
-            }}
+            style={style}
         >
             <NameInput onSetName={onSetName} />
         </Modal>
